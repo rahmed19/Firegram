cc1f3f022e569385a23a5195dd1ac7305aacdb1e
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-13 17:21:43 -0700

    hook setup

--- src/comps/upload-form.js
+++ src/comps/upload-form.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 export default function UploadForm() {
 
     const [file, setFile] = useState(null)
-    const [error, setError] = useState(null)
+    const [error, setError] = useState('')
 
     const types = ['image/png', 'image/jepg']
 
@@ -13,6 +13,7 @@ export default function UploadForm() {
         
         if (selected && types.includes(selected.type)) {
             setFile(selected)
+            setError('')
         } else {
             setFile(null)
             setError('Please select an image file (png or jpeg)')
@@ -23,6 +24,10 @@ export default function UploadForm() {
         <form>
             <input type='file' 
             onChange={handleChange}/>
+            <div className="output">
+                {error && <div className="error">{error}</div> }
+                {file && <div>{file.name.toUpperCase()}</div> }
+            </div>
         </form>
     )
 }
