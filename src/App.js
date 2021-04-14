7c628cfebc519915f186f7c3d589514f28dfc5e7
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-13 17:46:23 -0700

    Firebase import fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from './comps/Title';
+import Title from './comps/title';
 import UploadForm from './comps/upload-form';
 
 function App() {
---
+++ src/comps/progress-bar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseStorage from '../hooks/use-storage'
+
+export default function ProgressBar() {
+
+    return(
+        <>
+    
+        <div className="progress-bar">progress</div>
+        </>
+    )
+}
