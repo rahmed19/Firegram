b0e81cf8af04b9486e876d2d54201327676b2b05
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-14 19:54:18 -0700

    Images div

--- src/comps/image-grid.js
+++ src/comps/image-grid.js
@@ -7,7 +7,11 @@ export default function ImageGrid(){
 
     return(
         <div className="img-grid">
-            images
+            {docs && docs.map(doc => (
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} />
+                </div>
+            ))}
         </div>
     )
 }
