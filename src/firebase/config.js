41d44f7660d1cff2b292f94527bbf8dee52e1260
rahmed19 <59867027+rahmed19@users.noreply.github.com>
2021-04-14 21:38:07 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@
   
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyBnazeGwdX6ODq2chrzXrdJMbtPopVfkDg",
-    authDomain: "firegram-264d4.firebaseapp.com",
-    projectId: "firegram-264d4",
-    storageBucket: "firegram-264d4.appspot.com",
-    messagingSenderId: "622287934251",
-    appId: "1:622287934251:web:f6c9384c0b5988e8212b6e"
+   
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
